8fbe7c93fa6a39817f6caa9a2cdd6517c431bc99
Oshayer-Siddique <65402474+Oshayer-Siddique@users.noreply.github.com>
2024-06-04 16:18:28 +0600

    first

---
+++ src/Components/Incomes/Incomes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Incomes() {
+  return (
+    <IncomeStyled>Incomes</IncomeStyled>
+  )
+}
+
+const IncomeStyled = styled.div`
+
+`;
+
+
+
+export default Incomes
